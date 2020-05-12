afece2518729423e8cc1a4b516d27293064cf880
Michele Masina <masinamichele@gmail.com>
2020-05-12 15:19:28 +0200

    added new game on end

--- app.js
+++ app.js
@@ -9,102 +9,119 @@ const readline = require("readline");
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
+function quit() {
+  console.log("\n");
+  console.log("Bye!");
+  process.exit();
+}
+
 process.stdin.on("keypress", (str, key) => {
   if (key.ctrl && key.name == "x") {
-    console.log("\n");
-    console.log("Bye!");
-    process.exit();
+    quit();
   }
 });
 
 ~(async () => {
-  let GAME_STATE = "NEW";
-
-  utils.clear();
-
-  const score = new Scoreboard();
-  score.print();
-
-  const roll = new Roll();
-
-  console.log();
-  await prompts([
-    {
-      type: "invisible",
-      message: `Press ${c.cyan("Enter")} to start a new game.`,
-    },
-  ]);
-
-  let currentRound = 1;
-
-  const redraw = (currentRoll, reroll = true) => {
+  while (true) {
     utils.clear();
-    if (reroll) roll.roll();
-    score.preview(roll.getValue());
+
+    const score = new Scoreboard();
     score.print();
+
+    const roll = new Roll();
+
     console.log();
-    if (!currentRoll) return;
-    process.stdout.write(
-      ` Roll ${c.bold[[null, "green", "yellow", "red"][currentRoll]](
-        currentRoll
-      )}/3`
-    );
-    process.stdout.write(" ".repeat(32));
-    process.stdout.write(c.grey(`[${roll.getValue()}]`));
-    console.log();
-    roll.print();
-    console.log();
-  };
-
-  while (currentRound <= 13) {
-    let currentRoll = 1;
-    redraw(currentRoll);
-
-    while (true) {
-      const { diceKeep } = await prompts([
-        {
-          type: "number",
-          name: "diceKeep",
-          message: `Press ${c.cyan("1-5")} to keep dices. Leave empty to ${
-            currentRoll == 3 ? "Confirm" : "Roll"
-          }.`,
-          min: 1,
-          max: 6,
-        },
-      ]);
-
-      if (diceKeep) {
-        roll[`d${diceKeep}`].toggle();
-        redraw(currentRoll, false);
-      } else {
-        if (currentRoll <= 2) {
-          currentRoll++;
-          redraw(currentRoll);
-          if (currentRoll == 3) break;
-        } else break;
-      }
-    }
-
-    roll.deselect();
-    redraw(currentRoll, false);
-
-    const { scoreSel } = await prompts([
+    await prompts([
       {
-        type: "select",
-        name: "scoreSel",
-        message: `Select a score`,
-        choices: score.playableScores.map((s) => ({
-          title: s.name,
-          value: s.name,
-        })),
+        type: "invisible",
+        message: `Press ${c.cyan("Enter")} to start a new game.`,
       },
     ]);
 
-    score.score(roll.getValue()).on(scoreSel);
-    currentRound++;
+    let currentRound = 1;
+
+    const redraw = (currentRoll, reroll = true) => {
+      utils.clear();
+      if (reroll) roll.roll();
+      score.preview(roll.getValue());
+      score.print();
+      console.log();
+      if (!currentRoll) return;
+      process.stdout.write(
+        ` Roll ${c.bold[[null, "green", "yellow", "red"][currentRoll]](
+          currentRoll
+        )}/3`
+      );
+      process.stdout.write(" ".repeat(32));
+      process.stdout.write(c.grey(`[${roll.getValue()}]`));
+      console.log();
+      roll.print();
+      console.log();
+    };
+
+    while (currentRound <= 13) {
+      let currentRoll = 1;
+      redraw(currentRoll);
+
+      while (true) {
+        const { diceKeep } = await prompts([
+          {
+            type: "number",
+            name: "diceKeep",
+            message: `Press ${c.cyan("1-5")} to keep dices. Leave empty to ${
+              currentRoll == 3 ? "Confirm" : "Roll"
+            }.`,
+            min: 1,
+            max: 6,
+          },
+        ]);
+
+        if (diceKeep) {
+          roll[`d${diceKeep}`].toggle();
+          redraw(currentRoll, false);
+        } else {
+          if (currentRoll <= 2) {
+            currentRoll++;
+            redraw(currentRoll);
+            if (currentRoll == 3) break;
+          } else break;
+        }
+      }
+
+      roll.deselect();
+      redraw(currentRoll, false);
+
+      const { scoreSel } = await prompts([
+        {
+          type: "select",
+          name: "scoreSel",
+          message: `Select a score`,
+          choices: score.playableScores.map((s) => ({
+            title: s.name,
+            value: s.name,
+          })),
+        },
+      ]);
+
+      score.score(roll.getValue()).on(scoreSel);
+      currentRound++;
+    }
+
+    redraw(null, false);
+
+    console.log(` Game over. Score: ${score.getTotalScore()}\n`);
+
+    const { newGame } = await prompts([
+      {
+        type: "toggle",
+        name: "newGame",
+        message: "Play again?",
+        initial: true,
+        active: "Yes",
+        inactive: "No",
+      },
+    ]);
+
+    if (!newGame) quit();
   }
-
-  redraw(null, false);
-
-  console.log(`Game over. Score: ${score.getTotalScore()}\n`);
 })();
--- utils.js
+++ utils.js
@@ -11,10 +11,11 @@ const printLogo = () => {
    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝
   `);
   console.log(
-    ` Developed by Masina Michele - 2020                   v.${pjson.version}`
+    ` Developed by Masina Michele - 05/2020                v.${pjson.version}`
   );
   console.log(
-    c.gray(" https://en.wikipedia.org/wiki/Yahtzee") + "       Press ^X to quit"
+    c.gray(" https://en.wikipedia.org/wiki/Yahtzee") +
+      `               Quit: ${c.inverse("^X")}`
   );
   console.log();
 };
